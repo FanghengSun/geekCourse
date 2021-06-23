437039a2fb2509a2cb7b6913f15ead7c4a60911c
Fangheng.Sun <fangheng.sun@huilianyi.com>
2021-06-23 23:45:21 +0800

    配置git

---
+++ src/main/java/com/geek/course/geekcourse/GeekCourseApplication.java
@@ -0,0 +1,13 @@
+package com.geek.course.geekcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geek/course/geekcourse/GeekCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.course.geekcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
